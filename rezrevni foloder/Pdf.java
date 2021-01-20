ac78480d82ad9303f156165af8a9e99dfc4eab61
drasko1959 <drasko.kosovic@gmail.com>
2021-01-20 09:25:11 +0100

    init

---
+++ rezrevni foloder/Pdf.java
@@ -0,0 +1,143 @@
+package kupac.web.rest;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPrintServiceExporter;
+import net.sf.jasperreports.engine.export.JRPrintServiceExporterParameter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import kupac.domain.Korpa;
+import kupac.repository.KorpaRepository;
+
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.*;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.print.PrinterJob;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = ("*"))
+@RestController
+@RequestMapping("/report")
+public class Pdf {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    KorpaRepository KorpaRepository;
+
+    @GetMapping(path = "/korpa/{cijena}")
+    @ResponseBody
+
+    public void getPdfCijena(HttpServletResponse response, @PathVariable Double cijena) throws Exception {
+
+        Resource resource = context.getResource("classpath:reports/Artikli.jrxml");
+
+        InputStream inputStream = resource.getInputStream();
+        JasperReport report = JasperCompileManager.compileReport(inputStream);
+
+        Map<String, Object> params = new HashMap<>();
+
+        List<Korpa> korpa = KorpaRepository.findByCijena(cijena);
+
+        // Data source Set
+        JRDataSource dataSource = new JRBeanCollectionDataSource(korpa);
+        params.put("datasource", dataSource);
+
+        // Make jasperPrint
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        // Media Type
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        // Export PDF Stream
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    }
+
+    @GetMapping(path = "/korpa/{artikal}/{cijena}")
+    @ResponseBody
+
+    public void getPdfArtikalCijena(HttpServletResponse response, @PathVariable String artikal, @PathVariable Double cijena)
+            throws Exception {
+
+        Resource resource = context.getResource("classpath:reports/Artikli.jrxml");
+
+        InputStream inputStream = resource.getInputStream();
+        JasperReport report = JasperCompileManager.compileReport(inputStream);
+
+        Map<String, Object> params = new HashMap<>();
+
+        List<Korpa> korpa = (List<Korpa>) KorpaRepository.findByArikalCijena(artikal, cijena);
+
+        // Data source Set
+        JRDataSource dataSource = new JRBeanCollectionDataSource(korpa);
+        params.put("datasource", dataSource);
+
+        // Make jasperPrint
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        // Media Type
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        // Export PDF Stream
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    }
+
+    @GetMapping(path = "/korpa")
+    @ResponseBody
+
+    public void getPdfAll(HttpServletResponse response) throws Exception {
+
+        Resource resource = context.getResource("classpath:reports/Artikli.jrxml");
+
+        InputStream inputStream = resource.getInputStream();
+        JasperReport report = JasperCompileManager.compileReport(inputStream);
+
+        Map<String, Object> params = new HashMap<>();
+
+        List<Korpa> korpa = (List<Korpa>) KorpaRepository.findAll();
+
+        // Data source Set
+        JRDataSource dataSource = new JRBeanCollectionDataSource(korpa);
+        params.put("datasource", dataSource);
+
+        // Make jasperPrint
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        // Media Type
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        // Export PDF Stream
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    }
+
+    @GetMapping(path = "/korpa/{artikal}")
+    @ResponseBody
+
+    public void getPdfKorpaArtikal(HttpServletResponse response, @PathVariable String artikal) throws Exception {
+
+        Resource resource = context.getResource("classpath:reports/Artikli.jrxml");
+
+        InputStream inputStream = resource.getInputStream();
+        JasperReport report = JasperCompileManager.compileReport(inputStream);
+
+        Map<String, Object> params = new HashMap<>();
+
+        List<Korpa> korpa = KorpaRepository.findByArtikal(artikal);
+
+        // Data source Set
+        JRDataSource dataSource = new JRBeanCollectionDataSource(korpa);
+        params.put("datasource", dataSource);
+
+        // Make jasperPrint
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        // Media Type
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        // Export PDF Stream
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    }
+
+}
---
+++ rezrevni foloder/korpa.component.ts
@@ -0,0 +1,182 @@
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, ParamMap, Router, Data } from '@angular/router';
+import { Subscription, combineLatest } from 'rxjs';
+import { JhiEventManager } from 'ng-jhipster';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { IKorpa } from 'app/shared/model/korpa.model';
+
+import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
+import { KorpaService } from './korpa.service';
+import { KorpaDeleteDialogComponent } from './korpa-delete-dialog.component';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'jhi-korpa',
+  templateUrl: './korpa.component.html',
+  styleUrls: ['./korpa.component.scss'],
+})
+export class KorpaComponent implements OnInit, OnDestroy {
+  korpas?: IKorpa[];
+  eventSubscriber?: Subscription;
+  totalItems = 0;
+  itemsPerPage = ITEMS_PER_PAGE;
+  page!: number;
+  predicate!: string;
+  ascending!: boolean;
+  ngbPaginationPage = 1;
+  artikal: any;
+  cijena: any;
+
+  constructor(
+    protected korpaService: KorpaService,
+    protected activatedRoute: ActivatedRoute,
+    protected router: Router,
+    protected eventManager: JhiEventManager,
+    protected modalService: NgbModal,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+  loadPage(page?: number, dontNavigate?: boolean): void {
+    const pageToLoad: number = page || this.page || 1;
+
+    this.korpaService
+      .query({
+        page: pageToLoad - 1,
+        size: this.itemsPerPage,
+        sort: this.sort(),
+      })
+      .subscribe(
+        (res: HttpResponse<IKorpa[]>) => this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate),
+        () => this.onError()
+      );
+  }
+
+  ngOnInit(): void {
+    this.handleNavigation();
+    this.registerChangeInKorpas();
+  }
+
+  protected handleNavigation(): void {
+    combineLatest(this.activatedRoute.data, this.activatedRoute.queryParamMap, (data: Data, params: ParamMap) => {
+      const page = params.get('page');
+      const pageNumber = page !== null ? +page : 1;
+      const sort = (params.get('sort') ?? data['defaultSort']).split(',');
+      const predicate = sort[0];
+      const ascending = sort[1] === 'asc';
+      if (pageNumber !== this.page || predicate !== this.predicate || ascending !== this.ascending) {
+        this.predicate = predicate;
+        this.ascending = ascending;
+        this.loadPage(pageNumber, true);
+      }
+    }).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventSubscriber) {
+      this.eventManager.destroy(this.eventSubscriber);
+    }
+  }
+
+  trackId(index: number, item: IKorpa): number {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    return item.id!;
+  }
+
+  registerChangeInKorpas(): void {
+    this.eventSubscriber = this.eventManager.subscribe('korpaListModification', () => this.loadPage());
+  }
+
+  delete(korpa: IKorpa): void {
+    const modalRef = this.modalService.open(KorpaDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.korpa = korpa;
+  }
+
+  sort(): string[] {
+    const result = [this.predicate + ',' + (this.ascending ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  protected onSuccess(data: IKorpa[] | null, headers: HttpHeaders, page: number, navigate: boolean): void {
+    this.totalItems = Number(headers.get('X-Total-Count'));
+    this.page = page;
+    if (navigate) {
+      this.router.navigate(['/korpa'], {
+        queryParams: {
+          page: this.page,
+          size: this.itemsPerPage,
+          sort: this.predicate + ',' + (this.ascending ? 'asc' : 'desc'),
+        },
+      });
+    }
+    this.korpas = data || [];
+    this.ngbPaginationPage = this.page;
+  }
+
+  protected onError(): void {
+    this.ngbPaginationPage = this.page ?? 1;
+  }
+
+  artikalSearch(page?: number, dontNavigate?: boolean): void {
+    const pageToLoad: number = page || this.page || 1;
+
+    this.korpaService
+      .query({
+        'artikal.in': this.artikal,
+        page: pageToLoad - 1,
+        size: this.itemsPerPage,
+        sort: this.sort(),
+      })
+      .subscribe(
+        (res: HttpResponse<IKorpa[]>) => this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate),
+        () => this.onError()
+      );
+  }
+
+  cijenaSearch(page?: number, dontNavigate?: boolean): void {
+    const pageToLoad: number = page || this.page || 1;
+
+    this.korpaService
+      .query({
+        'cijena.in': this.cijena,
+        page: pageToLoad - 1,
+        size: this.itemsPerPage,
+        sort: this.sort(),
+      })
+      .subscribe(
+        (res: HttpResponse<IKorpa[]>) => this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate),
+        () => this.onError()
+      );
+  }
+  prazansearchArtikal(): void {
+    this.artikal = '';
+    this.cijenaSearch;
+  }
+  prazansearchCijena(): void {
+    this.cijena = '';
+    this.artikalSearch();
+  }
+
+  reportArtikal(): any {
+    if (this.artikal === undefined) {
+      window.open('http://localhost:8080/report/korpa', '_blank');
+    } else {
+      this.korpaService.reportServiceArtikal(this.artikal).subscribe((response: BlobPart) => {
+        const file = new Blob([response], { type: 'application/pdf' });
+        const fileURL = URL.createObjectURL(file);
+        window.open(fileURL);
+      });
+    }
+  }
+
+  exelArtikal(): void {
+    if (this.artikal === undefined) {
+      this.document.location.href = 'http://localhost:8080/excel/download';
+    } else {
+      this.document.location.href = 'http://localhost:8080/excel/download/artikal/' + this.artikal;
+    }
+  }
+}
