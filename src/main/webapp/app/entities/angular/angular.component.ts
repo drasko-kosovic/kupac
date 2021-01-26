d008c849f733e8e04b4665f7d35a322d24e1c424
kosovic1959 <drasko.kosovic@gmail.com>
2021-01-26 22:50:15 +0100

    init

--- src/main/webapp/app/entities/angular/angular.component.ts
+++ src/main/webapp/app/entities/angular/angular.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jhi-angular',
   templateUrl: './angular.component.html',
+  styleUrls: ['angular.component.scss'],
 })
 export class AngularComponent {
   constructor() {}
